cd05d36355b3f793879104ec991d8a9db83a2894
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-03-01 18:51:09 +0200

    autoproperties and immutability

---
+++ cs6/autoproperties and immutability/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoproperties_and_immutability
+{
+    class Character
+    {
+        /*private readonly int armor;
+
+        public int Armor
+        {
+            get { return armor; }
+        }
+
+        private void BadSpell()
+        {
+            //Armor = -1;
+        }*/
+
+        public int Armor { get; } = 100;
+
+        public Character()
+        {
+            Armor = 90; // this will have priority
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var character = new Character();
+            Console.WriteLine(character.Armor);
+        }
+    }
+}
