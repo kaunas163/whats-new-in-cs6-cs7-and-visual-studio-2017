f6ea49b2237e292b47d0bab41ab55db420ac717f
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-03-01 19:13:16 +0200

    nameof

---
+++ cs6/nameof_example/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using nameof_example.Annotations;
+
+namespace nameof_example
+{
+    public class Character : INotifyPropertyChanged
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value; 
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public Character() { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
