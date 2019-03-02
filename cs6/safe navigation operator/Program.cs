a5fbb05163a22a6aa10acd312d4206a7052e3454
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-03-02 13:02:21 +0200

    safe navigation operator

---
+++ cs6/safe navigation operator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace safe_navigation_operator
+{
+    public class Weapon
+    {
+        public string Name { get; }
+
+        public Weapon(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Character
+    {
+        public Weapon Weapon { get; }
+    }
+
+    class Program
+    {
+        static bool ArmedByKnife(Character c)
+        {
+            //return c != null && c.Weapon != null && c.Weapon.Name == "Knife";
+            return c?.Weapon?.Name == "Knife";
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
