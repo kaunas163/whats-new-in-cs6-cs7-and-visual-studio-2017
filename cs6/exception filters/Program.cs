bb4014958bc9d2a5f0f4b58c542c5025b25b9b84
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-03-02 13:20:32 +0200

    exception filters

---
+++ cs6/exception filters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception_filters
+{
+    class Program
+    {
+        private static bool Filter(ArgumentException ex)
+        {
+            return ex.Message == "bang";
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new ArgumentException("bang");
+            }
+            catch (ArgumentException ex) when (Filter(ex))
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
