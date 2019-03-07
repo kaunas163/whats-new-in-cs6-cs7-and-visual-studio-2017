5a55a4caa8579e2fe5a34581dccd98e33ffe0185
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-03-07 18:39:32 +0200

    local functions

---
+++ cs7/local functions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace local_functions
+{
+    public class Customer
+    {
+        public string GetFullInfo(string fullStr)
+        {
+            return GetFullName(fullStr).firstName + GetFullName(fullStr).lastName;
+
+            (string firstName, string lastName) GetFullName(string str)
+            {
+                return (str.Split()[0], str.Split()[1]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
