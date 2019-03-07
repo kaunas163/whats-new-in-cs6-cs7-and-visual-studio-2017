9ce7f9fb9accf9df96cf15e763ae1f0665dbf096
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-03-07 18:51:34 +0200

    improvements to literals

---
+++ cs7/improvements to literals/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace improvements_to_literals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cash = 1_387_254_00;
+            Console.WriteLine(cash);
+
+            int bitMask = 0b1000_1010_0010;
+            Console.WriteLine(bitMask);
+        }
+    }
+}
