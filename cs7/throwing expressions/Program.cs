a0307de3a03f1f69eaba0fb6d8af60e16f73b67e
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-03-07 18:48:55 +0200

    throwing expressions

---
+++ cs7/throwing expressions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace throwing_expressions
+{
+    public class Customer
+    {
+        public string GetSurname(string fullName)
+        {
+            var names = fullName.Split();
+            return names.Length > 1 ? names[1] : throw new ArgumentException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
